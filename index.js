175279461cdf2d41851e077e9b0010c8c456796c
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2021-05-30 02:00:00 +0530

    seeding campgrounds

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require('express')
-const app = express()
 const path = require('path')
+const mongoose = require('mongoose');
 const Campground = require('./models/campground')
 
-const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -15,6 +14,8 @@ db.on('error', console.error.bind(console, 'connection error:'))
 db.once('open', () => {
     console.log('Database connected')
 })
+
+const app = express()
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
