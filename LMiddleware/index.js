6bc95018d3604b9b4b6a7d00582810fd50b03ec6
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2021-05-30 19:37:54 +0530

    defining custom middleware and using next function

--- LMiddleware/index.js
+++ LMiddleware/index.js
@@ -5,11 +5,22 @@ const morgan = require('morgan')
 
 // middleware
 // app.use(morgan('dev'))
-app.use(morgan('common'))
+app.use(morgan('dev'))
 
 // app.use(() => {
 //     console.log("heyyy")
 // })
+
+app.use((req, res, next) => {
+    console.log("THIS IS MY FIRST MIDDLEWARE")
+    return next()
+    console.log("THIS IS MY FIRST MIDDLEWARE after calling next")
+
+})
+app.use((req, res, next) => {
+    console.log("THIS IS MY second MIDDLEWARE")
+    next()
+})
 
 app.get('/', (req, res) => {
     res.send('Welcome Home!')
