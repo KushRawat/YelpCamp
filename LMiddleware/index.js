8ec40f8fe4ca6d69c10d5216af5a440057a62b6a
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2021-05-30 21:25:14 +0530

    setting up 404 route

--- LMiddleware/index.js
+++ LMiddleware/index.js
@@ -3,10 +3,16 @@ const app = express()
 const morgan = require('morgan')
 
 app.use(morgan('dev'))
+
 app.use((req, res, next) => {
     req.requestTime = Date.now()
     // req.method = 'GET'
     console.log(req.method.toLowerCase(), req.path)
+    next()
+})
+// Will work irrespective of being a get req or post req
+app.use('/dogs', (req, res, next) => {
+    console.log("I LOVE DOGS!!")
     next()
 })
 
@@ -35,6 +41,11 @@ app.get('/', (req, res) => {
 app.get('/dogs', (req, res) => {
     console.log(`REQUEST DATE: ${req.requestTime}`)
     res.send('Woof Woof!')
+})
+
+app.use((req, res) => {
+    // res.send('NOT FOUND')
+    res.status(404).send('NOT FOUND!')
 })
 
 app.listen(3000, () => {
