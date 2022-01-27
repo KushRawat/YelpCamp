1983e28ab9f1e19159c283764c6d51d563967b44
Kush Rawat <rawatkush0704@gmail.com>
2022-01-27 05:36:16 +0530

    using try and catch for async error handling

--- LHandlingAsyncErrors/MongooseExpress/index.js
+++ LHandlingAsyncErrors/MongooseExpress/index.js
@@ -8,14 +8,14 @@ const AppError = require("./AppError");
 const Product = require("./models/product");
 
 mongoose
-  .connect("mongodb://localhost:27017/farmStand2", { useNewUrlParser: true })
-  .then(() => {
-    console.log("MONGO CONNECTION OPEN!!");
-  })
-  .catch((err) => {
-    console.log("MONGO CONNECTION ERROR!");
-    console.log(err);
-  });
+    .connect("mongodb://localhost:27017/farmStand2", { useNewUrlParser: true })
+    .then(() => {
+        console.log("MONGO CONNECTION OPEN!!");
+    })
+    .catch((err) => {
+        console.log("MONGO CONNECTION ERROR!");
+        console.log(err);
+    });
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -28,60 +28,72 @@ const categories = ["fruit", "vegetable", "dairy"];
 
 // USING REST & CRUD
 app.get("/products", async (req, res) => {
-  const products = await Product.find({});
-  res.render("products/index", { products });
+    const products = await Product.find({});
+    res.render("products/index", { products });
 });
 // C
 app.get("/products/new", (req, res) => {
-  res.render("products/new", { categories });
+    res.render("products/new", { categories });
 });
 
-app.post("/products", async (req, res) => {
-  const newProduct = new Product(req.body);
-  await newProduct.save();
-  res.redirect(`/products/${newProduct._id}`);
+app.post("/products", async (req, res, next) => {
+    try {
+        const newProduct = new Product(req.body);
+        await newProduct.save();
+        res.redirect(`/products/${newProduct._id}`);
+    } catch (err) {
+        return next(err);
+    }
 });
 // R
 app.get("/products/:id", async (req, res, next) => {
-  const { id } = req.params;
-  const foundProduct = await Product.findById(id);
-  if (!foundProduct) {
-    return next(new AppError("Product not found", 404));
-  }
-  console.log(foundProduct);
-  res.render("products/show", { foundProduct });
+    const { id } = req.params;
+    const foundProduct = await Product.findById(id);
+    if (!foundProduct) {
+        return next(new AppError("Product not found", 404));
+    }
+    console.log(foundProduct);
+    res.render("products/show", { foundProduct });
 });
 // U
 app.get("/products/:id/edit", async (req, res, next) => {
-  const { id } = req.params;
-  const foundProduct = await Product.findById(id);
-  if (!foundProduct) {
-    return next(new AppError("Product not found", 404));
-  }
-  res.render("products/edit", { foundProduct, categories });
+    try {
+        const { id } = req.params;
+        const foundProduct = await Product.findById(id);
+        if (!foundProduct) {
+            throw new AppError("Product not found", 404);
+        }
+        res.render("products/edit", { foundProduct, categories });
+    } catch (err) {
+        next(err);
+    }
 });
 
-app.put("/products/:id", async (req, res) => {
-  const { id } = req.params;
-  const updateProduct = await Product.findByIdAndUpdate(id, req.body, {
-    runValidators: true,
-    new: true,
-  });
-  res.redirect(`/products/${updateProduct._id}`);
+app.put("/products/:id", async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const updateProduct = await Product.findByIdAndUpdate(id, req.body, {
+            runValidators: true,
+            new: true,
+        });
+        res.redirect(`/products/${updateProduct._id}`);
+    } catch (err) {
+        next(err);
+    }
 });
 // D
 app.delete("/products/:id", async (req, res) => {
-  const { id } = req.params;
-  const deletedProduct = await Product.findByIdAndDelete(id);
-  res.redirect("/products");
+    const { id } = req.params;
+    const deletedProduct = await Product.findByIdAndDelete(id);
+    res.redirect("/products");
 });
 
 // BASIC ERROR HANDLING MIDDLEWARE
 app.use((err, req, res, next) => {
-  const { status = 500, message = "Something went wrong" } = err;
-  res.status(status).send(message);
+    const { status = 500, message = "Something went wrong" } = err;
+    res.status(status).send(message);
 });
 
 app.listen(3000, () => {
-  console.log("APP IS LISTENING ON PORT 3000");
+    console.log("APP IS LISTENING ON PORT 3000");
 });
