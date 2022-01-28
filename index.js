f0c2df3c8f261b6da94dfa6b72fcce373b9a85f5
Kush Rawat <rawatkush0704@gmail.com>
2022-01-28 06:11:35 +0530

    JOI Schema Validation Server-Side

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
+const Joi = require("joi");
 const catchAsync = require("./Utilities/catchAsync");
 const ExpressError = require("./Utilities/ExpressError");
 const methodOverride = require("method-override");
@@ -47,8 +48,24 @@ app.get("/campgrounds/new", (req, res) => {
 app.post(
     "/campgrounds",
     catchAsync(async (req, res, next) => {
-        if (!req.body.campground)
-            throw new ExpressError("Invalid Campground Data", 400);
+        // if (!req.body.campground)
+        //     throw new ExpressError("Invalid Campground Data", 400);
+        const campgroundSchema = Joi.object({
+            campground: Joi.object({
+                title: Joi.string().required(),
+                image: Joi.string().required(),
+                price: Joi.number().required(),
+                description: Joi.string().required(),
+                location: Joi.string().required(),
+            }).required(),
+        });
+        const { error } = campgroundSchema.validate(req.body);
+        // console.log(result);
+        if (error) {
+            const msg = error.details.map((ele) => ele.message);
+            throw new ExpressError(msg, 400);
+        }
+
         const campground = new Campground(req.body.campground);
         await campground.save();
         res.redirect(`/campgrounds/${campground._id}`);
