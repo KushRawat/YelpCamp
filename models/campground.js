0d9ef58c502f9875f5986748ec522cf755bb4781
Kush Rawat <rawatkush0704@gmail.com>
2022-02-05 04:16:17 +0530

    review model added

--- models/campground.js
+++ models/campground.js
@@ -1,14 +1,15 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const CampgroundsSchema = new Schema({
     title: String,
     image: String,
     price: Number,
     description: String,
-    location: String
-})
+    location: String,
+    reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: "Review" }],
+});
 
-const Campground = mongoose.model('Campground', CampgroundsSchema)
+const Campground = mongoose.model("Campground", CampgroundsSchema);
 
-module.exports = Campground
+module.exports = Campground;
