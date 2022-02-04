ca9053ed105d352905858ec2d928fc03ebb9996e
Kush Rawat <rawatkush0704@gmail.com>
2022-02-04 20:54:11 +0530

    farm and product relationship integrated

--- LHandlingAsyncErrors/MongooseExpress/index.js
+++ LHandlingAsyncErrors/MongooseExpress/index.js
@@ -37,13 +37,15 @@ app.get("/farms/new", (req, res) => {
 });
 
 app.get("/farms/:id", async (req, res) => {
-    const farm = await Farm.findById(req.params.id);
+    const farm = await Farm.findById(req.params.id).populate("products");
+    console.log(farm);
     res.render("farms/show", { farm });
 });
 
 app.get("/farms/:id/products/new", async (req, res) => {
     const { id } = req.params;
-    res.render("products/new", { categories, id });
+    const farm = await Farm.findById(id);
+    res.render("products/new", { categories, farm });
 });
 
 app.post("/farms/:id/products", async (req, res) => {
@@ -56,7 +58,7 @@ app.post("/farms/:id/products", async (req, res) => {
     product.farm = farm;
     farm.save();
     product.save();
-    res.send(farm);
+    res.redirect(`/farms/${id}`);
     // farm.products.push(product)
 });
 
@@ -92,7 +94,7 @@ app.post("/products", async (req, res, next) => {
 // R
 app.get("/products/:id", async (req, res, next) => {
     const { id } = req.params;
-    const foundProduct = await Product.findById(id);
+    const foundProduct = await Product.findById(id).populate('farm','name');
     if (!foundProduct) {
         return next(new AppError("Product not found", 404));
     }
