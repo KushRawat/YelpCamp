d63c5e33d08adaacb643db1f54e845c70541a090
Kush Rawat <rawatkush0704@gmail.com>
2022-02-05 20:13:23 +0530

    review route/api added

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const catchAsync = require("./Utilities/catchAsync");
 const ExpressError = require("./Utilities/ExpressError");
 const methodOverride = require("method-override");
 const Campground = require("./models/campground");
+const Review = require("./models/review");
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
     useNewUrlParser: true,
@@ -104,6 +105,18 @@ app.delete(
         const { id } = req.params;
         const campground = await Campground.findByIdAndDelete(id);
         res.redirect("/campgrounds");
+    })
+);
+
+app.post(
+    "/campgrounds/:id/reviews",
+    catchAsync(async (req, res) => {
+        const campground = await Campground.findById(req.params.id);
+        const review = new Review(req.body.review)
+        campground.reviews.push(review)
+        review.save()
+        campground.save()
+        res.redirect(`/campgrounds/${campground._id}`)
     })
 );
 
