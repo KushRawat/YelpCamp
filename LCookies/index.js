e391d4171030a779cc346d8283c8103cb9d28ce6
Kush Rawat <rawatkush0704@gmail.com>
2022-02-06 16:04:34 +0530

    cookie-parser middleware

--- LCookies/index.js
+++ LCookies/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
 
 app.use("/greet", (req, res) => {
-    res.send("HELLOOO");
+    // console.log(req.cookies);
+    const { name = "yolo" } = req.cookies;
+    res.send(`Hello ${name}`);
 });
 
 app.use("/sendname", (req, res) => {
