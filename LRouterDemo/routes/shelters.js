2f386b0d3341b63393a2ecc89781258c4f9622f9
Kush Rawat <rawatkush0704@gmail.com>
2022-02-06 09:13:14 +0530

    express router

---
+++ LRouterDemo/routes/shelters.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.send("ALL SHELTERS");
+});
+router.post("/", (req, res) => {
+    res.send("create shelters");
+});
+router.get("/:id", (req, res) => {
+    res.send("one SHELTERS");
+});
+router.get("/:id/edit", (req, res) => {
+    res.send("edit one SHELTERS");
+});
+
+module.exports = router;
