f348521acad2f1b70702e0e9708dd1691984b0b5
Kush Rawat <rawatkush0704@gmail.com>
2022-02-06 16:58:55 +0530

    signed cookies

--- LCookies/index.js
+++ LCookies/index.js
@@ -1,17 +1,26 @@
 const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
-app.use(cookieParser());
+app.use(cookieParser("mysecret"));
 
-app.use("/greet", (req, res) => {
+app.get("/greet", (req, res) => {
     // console.log(req.cookies);
     const { name = "yolo" } = req.cookies;
     res.send(`Hello ${name}`);
 });
 
-app.use("/sendname", (req, res) => {
+app.get("/sendname", (req, res) => {
     res.cookie("name", "yolo bhai");
     res.send("Cookie sent");
+});
+
+app.get("/getsignedcookie", (req, res) => {
+    res.cookie("fruit", "grape", { signed: true });
+    res.send("GET SIGNED COOKIE");
+});
+
+app.get("/verifyfruit", (req, res) => {
+    res.send(req.signedCookies);
 });
 
 app.listen(3000, () => {
