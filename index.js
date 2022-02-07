61d086040855f4274f9320d7eaee743b2830c1e0
Kush Rawat <rawatkush0704@gmail.com>
2022-02-07 19:38:46 +0530

    setting static assets

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ mongoose.connect("mongodb://localhost:27017/yelp-camp", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
+    useFindAndModify:false
 });
 
 const db = mongoose.connection;
@@ -28,6 +29,7 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/reviews", reviewRoutes);
