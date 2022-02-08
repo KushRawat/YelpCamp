f95fc61ebcd33c966d52e44b92d0d7bd825f2d63
Kush Rawat <rawatkush0704@gmail.com>
2022-02-08 22:55:02 +0530

    auth demo register

--- LAuthDemo/index.js
+++ LAuthDemo/index.js
@@ -1,14 +1,41 @@
 const express = require("express");
 const app = express();
-// const router = express.Router();
-
 const User = require("./models/users");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+mongoose
+    .connect("mongodb://localhost:27017/authDemo", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log("DATABASE CONNECTED");
+    })
+    .catch((err) => {
+        console.log("EROR CONNECTING TO DB");
+        console.log(err);
+    });
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-app.get("/register", async (req, res) => {
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.send("THIS IS HOME PAGE");
+});
+
+app.get("/register", (req, res) => {
     res.render("register");
+});
+
+app.post("/register", async (req, res) => {
+    const { username, password } = req.body;
+    const hash = await bcrypt.hash(password, 12);
+    const user = await new User({ username, password: hash });
+    await user.save();
+    res.redirect("/");
 });
 
 app.get("/secret", async (req, res) => {
