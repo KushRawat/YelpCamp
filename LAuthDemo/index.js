cb56d6efd66620705605041eca1c04b22081fe05
Kush Rawat <rawatkush0704@gmail.com>
2022-02-09 01:56:04 +0530

    staying logged in with session

--- LAuthDemo/index.js
+++ LAuthDemo/index.js
@@ -3,6 +3,7 @@ const app = express();
 const User = require("./models/users");
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const session = require("express-session");
 
 mongoose
     .connect("mongodb://localhost:27017/authDemo", {
@@ -21,6 +22,7 @@ app.set("view engine", "ejs");
 app.set("views", "views");
 
 app.use(express.urlencoded({ extended: true }));
+app.use(session({ secret: "thisisnotagoodsecret" }));
 
 app.get("/", (req, res) => {
     res.send("THIS IS HOME PAGE");
@@ -37,9 +39,10 @@ app.get("/login", (req, res) => {
 app.post("/register", async (req, res) => {
     const { username, password } = req.body;
     const hash = await bcrypt.hash(password, 12);
-    const user = await new User({ username, password: hash });
+    const user = new User({ username, password: hash });
     await user.save();
-    res.redirect("/");
+    req.session.user_id = user._id
+    res.redirect("/secret");
 });
 
 app.post("/login", async (req, res) => {
@@ -47,13 +50,20 @@ app.post("/login", async (req, res) => {
     const user = await User.findOne({ username });
     const validPassword = await bcrypt.compare(password, user.password);
     if (validPassword) {
-        res.send("LOGGED IN");
+        req.session.user_id = user._id;
+        res.redirect("/secret");
+        // res.send("LOGGED IN");
     } else {
-        res.send("WRONG CREDENTIALS");
+        res.redirect("/login");
+        // res.send("WRONG CREDENTIALS");
     }
 });
 
 app.get("/secret", async (req, res) => {
+    if (!req.session.user_id) {
+        return res.redirect("/login");
+    }
+    // console.log(req.session.user_id)
     res.send("This is a secret. you cant see me unitl you are logged in");
 });
 
