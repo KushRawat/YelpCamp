e4b4812bbfd2bcc0e29aef09309be1174edf94f4
Kush Rawat <rawatkush0704@gmail.com>
2022-02-10 03:37:51 +0530

    register route logic

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,27 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/user");
+const catchAsync = require("../Utilities/catchAsync");
 
 router.get("/register", (req, res) => {
     res.render("users/register");
 });
 
-router.post("/register", async (req, res) => {
-    res.send(req.body);
-});
+router.post(
+    "/register",
+    catchAsync(async (req, res) => {
+        try {
+            const { username, password, email } = req.body;
+            const user = new User({ username, email });
+            const registeredUser = await User.register(user, password);
+            console.log(registeredUser);
+            req.flash("success", "Welcome to YelpCamp");
+            res.redirect("/campgrounds");
+        } catch (err) {
+            req.flash("error", err.message);
+            res.redirect("/register");
+        }
+    })
+);
 
 module.exports = router;
