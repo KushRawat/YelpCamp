8b53cb5115f110b8583c50906163d6bf1b040900
Kush Rawat <rawatkush0704@gmail.com>
2022-02-11 04:04:59 +0530

    return to behavior

--- middleware.js
+++ middleware.js
@@ -1,6 +1,7 @@
 const isLoggedIn = (req, res, next) => {
     // console.log(req.user)
     if (!req.isAuthenticated()) {
+        req.session.returnTo = req.originalUrl;
         req.flash("error", "You must be signed in first!");
         return res.redirect("/login");
     }
