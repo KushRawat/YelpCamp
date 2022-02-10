243fb5a0932d6b3bf79c2d3d2d278aec1eb72f9a
Kush Rawat <rawatkush0704@gmail.com>
2022-02-11 02:39:06 +0530

    cuurent user middleware added

--- routes/campground.js
+++ routes/campground.js
@@ -23,7 +23,6 @@ const validateCampground = (req, res, next) => {
 
 router.get(
     "/",
-
     catchAsync(async (req, res) => {
         const campgrounds = await Campground.find({});
         res.render("campgrounds/index", { campgrounds });
