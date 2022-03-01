ae0efa890bfe0bc7fe340eac1c1e43d1da23e671
Kush Rawat <rawatkush0704@gmail.com>
2022-03-01 20:24:59 +0530

    MAPBOX-geocoding location & GeoJson

--- models/campground.js
+++ models/campground.js
@@ -1,46 +1,57 @@
-const mongoose = require("mongoose");
-const Review = require("./review");
+const mongoose = require('mongoose');
+const Review = require('./review');
 const Schema = mongoose.Schema;
 
 // https://res.cloudinary.com/douqbebwk/image/upload/w_300/v1600113904/YelpCamp/gxgle1ovzd2f3dgcpass.png
 
 const ImageSchema = new Schema({
-    url: String,
-    filename: String,
+  url: String,
+  filename: String,
 });
 
-ImageSchema.virtual("thumbnail").get(function () {
-    return this.url.replace("/upload", "/upload/w_200");
+ImageSchema.virtual('thumbnail').get(function () {
+  return this.url.replace('/upload', '/upload/w_200');
 });
 
 const CampgroundSchema = new Schema({
-    title: String,
-    images: [ImageSchema],
-    price: Number,
-    description: String,
-    location: String,
-    author: {
-        type: Schema.Types.ObjectId,
-        ref: "User",
+  title: String,
+  images: [ImageSchema],
+  geometry: {
+    type: {
+      type: String,
+      enum: ['Point'],
+      required: true,
     },
-    reviews: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: "Review",
-        },
-    ],
+    coordinates: {
+      type: [Number],
+      required: true,
+    },
+  },
+  price: Number,
+  description: String,
+  location: String,
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Review',
+    },
+  ],
 });
 
-CampgroundSchema.post("findOneAndDelete", async (campground) => {
-    if (campground.reviews.length) {
-        const res = await Review.deleteMany({
-            _id: { $in: campground.reviews },
-        });
-        // console.log(res);
-    }
-    // console.log(campground);
+CampgroundSchema.post('findOneAndDelete', async (campground) => {
+  if (campground.reviews.length) {
+    const res = await Review.deleteMany({
+      _id: { $in: campground.reviews },
+    });
+    // console.log(res);
+  }
+  // console.log(campground);
 });
 
-const Campground = mongoose.model("Campground", CampgroundSchema);
+const Campground = mongoose.model('Campground', CampgroundSchema);
 
 module.exports = Campground;
